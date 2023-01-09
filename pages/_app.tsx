97a942d0960c53a68d186de9ce605fc30e0ea6b7
Alberto Enriquez Enriquez <aenriquezenriquez@deloitte.es>
2023-01-09 22:42:14 +0100

    add/ganalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-S243ES1N9E"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-S243ES1N9E');
+        `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
